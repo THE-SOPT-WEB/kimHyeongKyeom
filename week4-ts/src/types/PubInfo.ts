5dedd871683e3e0ba1145ae4aef90b94565bf497
Brokyeom <goindol40@gmail.com>
2022-06-21 09:20:35 +0900

    refactor: 폴더 구조 변경 및 리팩터링, router 적용

---
+++ week4-ts/src/types/PubInfo.ts
@@ -0,0 +1,8 @@
+export interface PubInfo {
+    readonly id?: number;
+    readonly place_url?: string;
+    readonly place_name?: string;
+    readonly phone?: string;
+    readonly distance?: string;
+    readonly adress_name?: string;
+  }
